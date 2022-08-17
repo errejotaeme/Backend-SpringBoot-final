2795650263ac7fa212f6c960a9347385309fab03
Juan Manuel Rodríguez Miguel <rodriguezjuanma89@outlook.com>
2022-08-17 18:46:39 -0300

    prueba 15 cors

--- src/main/java/com/backend/portfolio/controller/AccesoController.java
+++ src/main/java/com/backend/portfolio/controller/AccesoController.java
@@ -20,9 +20,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
+@RequestMapping("/acceso")
 @CrossOrigin(origins = "https://front-portfolio-angular.web.app")
 public class AccesoController {
     
